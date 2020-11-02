0b276154b87a36a7c0b0af253beafa09a7f37330
Daniel.Hsiao <daniel.hsiao@os-is.com>
2020-11-02 01:59:28 +0100

    change to print out sub str going to replace

--- convert.R
+++ convert.R
@@ -9,14 +9,14 @@ MapTable[, id := as.character(id)]
 MapTable <- MapTable[!is.na(id), .(id, utf16string)]
 
 for(curId in MapTable$id){
+  if(id=='31298961')browser()
   # curId <- '28976' # example
   location <- stri_locate_all(pattern=paste0('\"',curId,'\":\"'), rawString, fixed = TRUE)[[1]]
   startPos <- unname(location[1,2]+1)
   endLocations <- stri_locate_all(pattern=paste0('\",\"'), rawString, fixed = TRUE)[[1]][,1]
   endPos <- unlist(endLocations[which(endLocations>startPos)[1]]-1)
-  # print(substr(rawString, startPos, endPos))
+  print(substr(rawString, startPos, endPos))
   rawString <- `stri_sub<-`(rawString, startPos, endPos, value=unlist(MapTable[id==curId,utf16string]))
-  # print(unlist(MapTable[id==curId,utf16string]))
 }
 
 write(rawString, "ModifiedEN")
