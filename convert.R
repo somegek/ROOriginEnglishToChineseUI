d1d51dd19bca42a90a349997b597fd9dbfcd3984
somegek <somegek@gmail.com>
2020-11-04 20:21:33 +0100

    Update convert.R

--- convert.R
+++ convert.R
@@ -24,7 +24,7 @@ rawString <- `stri_sub<-`(rawString,nchar(rawString), nchar(rawString), value = 
 
 write(rawString, "ModifiedEN")
 
-# repo <- git2r::init()
-# git2r::commit(message='0', all = TRUE)
+repo <- git2r::init()
+git2r::commit(message='0', all = TRUE)
 # git2r::config(repo, user.name = "Alice", user.email = "Alice@example.com")
 # git2r::push()
