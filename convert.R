6b65cba69b089681df35498ce1b5785620e73cb3
somegek <somegek@gmail.com>
2020-11-17 21:10:09 +0100

    0

--- convert.R
+++ convert.R
@@ -13,33 +13,37 @@ git2r::pull()
 startTime <- Sys.time()
 load(file='rawString.rdata')
 
+endString <- ",\"696473873\":\" \"}"
+rawString <- `stri_sub<-`(rawString,nchar(rawString), nchar(rawString), value = endString)
+
 KoId <- names(jsonlite::fromJSON(rawString))
 
 ChTable <- as.data.table(read_sheet("1zbRXgH4_XHXNLBqMakUkJ1gq6mAnSMiMunfgiWY-MIw"))
-ChTable[, id := as.character(id)]
+ChTable[, id := as.numeric(id)]
 ChTable <- ChTable[id %in% KoId,.(id,utf16string)]
 
 MapTable <- as.data.table(read_sheet("1IFXu-ybDMz5asDhDRFljB6ziE6WtPRzw9W9hodvnqLE"))
-MapTable[, id := as.character(id)]
+MapTable[, id := as.numeric(id)]
 MapTable <- MapTable[!is.na(id) & !is.na(utf16string), .(id, utf16string)]
 
-MapTable <- merge(ChTable, MapTable, by = "id", all.x = TRUE)
-MapTable[!is.na(utf16string.y), utf16string.x :=utf16string.y]
-MapTable[, utf16string.y:=NULL]
-setnames(MapTable, "utf16string.x","utf16string")
-
-for(curId in MapTable$id){
-  # curId <- '28976' # example
+NewTable <- merge(ChTable, MapTable, by = "id", all.x = TRUE)
+NewTable[!is.na(utf16string.y), utf16string.x :=utf16string.y]
+NewTable[, utf16string.y:=NULL]
+setnames(NewTable, "utf16string.x","utf16string")
+setorder(NewTable, id)
+nTotal <- nrow(NewTable)
+pos <- 0
+for(curId in NewTable$id){
+  pos <- pos+1
+  print(round(pos/nTotal,2))
+  # curId <- '4265649338' # example
   location <- stri_locate_all_fixed(pattern=paste0('\"',curId,'\":\"'), rawString)[[1]]
   startPos <- unname(location[1,2]+1)
   endLocations <- stri_locate_all_fixed(pattern='\",\"', rawString)[[1]][,1]
   endPos <- unlist(endLocations[which(endLocations>startPos)[1]]-1)
-  print(substr(rawString, startPos, endPos))
-  rawString <- `stri_sub<-`(rawString, startPos, endPos, value=unlist(MapTable[id==curId,utf16string]))
+  # print(substr(rawString, startPos, endPos))
+  rawString <- `stri_sub<-`(rawString, startPos, endPos, value=unlist(NewTable[id==curId,utf16string]))
 }
-
-endString <- ",\"696473873\":\" \"}"
-rawString <- `stri_sub<-`(rawString,nchar(rawString), nchar(rawString), value = endString)
 
 write(rawString, "ModifiedEN")
 
@@ -50,4 +54,4 @@ git2r::commit(message='0', all = TRUE)
 # git2r::config(repo, user.name = "Alice", user.email = "Alice@example.com")
 # git2r::push()
 endTime <- Sys.time()
-print(startTime-endTime)
+print(endTime-startTime)
