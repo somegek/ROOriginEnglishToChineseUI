ab4937a2cfb1a2c2284cf0c83addd1fb6de45fc5
somegek <somegek@gmail.com>
2020-11-23 14:28:34 +0100

    0

--- convert2.R
+++ convert2.R
@@ -13,15 +13,22 @@ git2r::pull()
 startTime <- Sys.time()
 load(file='rawString.rdata')
 
-endString <- ",\"696473873\":\" \"}"
-rawString <- `stri_sub<-`(rawString,nchar(rawString), nchar(rawString), value = endString)
 rawString <- substr(rawString, 2, nchar(rawString)-1)
 
-KoId <- names(jsonlite::fromJSON(rawString))
+stringVec <- strsplit(rawString,'\",\"',fixed = TRUE)[[1]]
+stringList <- lapply(stringVec, function(x){
+  x <- strsplit(x, '\":\"', fixed = TRUE)[[1]]
+  return(x)
+})
+stringList[[6]] <- c('12289', ',')
+stringList[[7]] <- c('19968', 'Monday')
+stringId <- unlist(lapply(stringList, function(x){
+  return(x[[1]])
+}))
 
 ChTable <- as.data.table(read_sheet("1zbRXgH4_XHXNLBqMakUkJ1gq6mAnSMiMunfgiWY-MIw"))
 ChTable[, id := as.numeric(id)]
-ChTable <- ChTable[id %in% KoId,.(id,utf16string)]
+ChTable <- ChTable[id %in% stringId,.(id,utf16string)]
 
 MapTable <- as.data.table(read_sheet("1IFXu-ybDMz5asDhDRFljB6ziE6WtPRzw9W9hodvnqLE"))
 MapTable[, id := as.numeric(id)]
@@ -32,36 +39,27 @@ NewTable[!is.na(utf16string.y), utf16string.x :=utf16string.y]
 NewTable[, utf16string.y:=NULL]
 setnames(NewTable, "utf16string.x","utf16string")
 setorder(NewTable, id)
-
-curId <- '3364070197' # example
-stringVec <- strsplit(rawString,'\",\"',fixed = TRUE)[[1]]
-stringList <- lapply(stringVec, function(x){
-  x <- strsplit(x, '\":\"', fixed = TRUE)[[1]]
-  return(x)
-})
-stringList[[6]] <- c('12289', ',')
-stringId <- unlist(lapply(stringList, function(x){
-  return(x[[1]])
-}))
+NewList <- as.list(transpose(NewTable))
 
 stringList <- stringList[which(!stringId %in% NewTable$id)]
 
-NewList <- as.list(transpose(NewTable))
+
+
+
 stringList <- c(stringList,NewList)
 
 stringVec <- lapply(stringList, function(x){
   return(paste0(x[1], '\":\"', x[2]))
 })
 
-rawString2 <- paste0(stringVec, collapse = '\",\"')
-rawString2 <- paste0("{", rawString2, "}")
+rawFinal <- paste0(stringVec, collapse = '\",\"')
+rawFinal <- paste0("{", rawFinal, "\"}")
 
-# rawString <- stri_replace_all_fixed(rawString, '\n','\\n')
-# save(rawString, file='modifiedString.RData')
+rawFinal <- stri_replace_all_fixed(rawFinal, '\n','\\n')
 
-write(rawString, "ModifiedEN")
+write(rawFinal, "ModifiedEN")
 
-# rawString <- stri_replace_all_fixed(rawString, '\",\"', '\",\n\"')
+rawString <- stri_replace_all_fixed(rawString, '\",\"', '\",\n\"')
 write(rawString, "ModifiedEN.txt")
 
 git2r::commit(message='0', all = TRUE)
