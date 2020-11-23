6ab234f6064ae4442a3e527ea01094dac53b6d9e
somegek <somegek@gmail.com>
2020-11-23 12:51:32 +0100

    Create convert2.R
    inital conver t

---
+++ convert2.R
@@ -0,0 +1,58 @@
+if(Sys.info()[['user']]=='arwin'){
+  source('C:/Users/arwin/Desktop/RO/ROOriginEnglishToChineseUI/changeLibPath.R', echo=FALSE)
+}
+
+library(data.table)
+library(googlesheets4)
+library(stringi)
+library(progress)
+library(git2r)
+repo <- git2r::init()
+git2r::pull()
+
+startTime <- Sys.time()
+load(file='rawString.rdata')
+
+endString <- ",\"696473873\":\" \"}"
+rawString <- `stri_sub<-`(rawString,nchar(rawString), nchar(rawString), value = endString)
+
+KoId <- names(jsonlite::fromJSON(rawString))
+
+ChTable <- as.data.table(read_sheet("1zbRXgH4_XHXNLBqMakUkJ1gq6mAnSMiMunfgiWY-MIw"))
+ChTable[, id := as.numeric(id)]
+ChTable <- ChTable[id %in% KoId,.(id,utf16string)]
+
+MapTable <- as.data.table(read_sheet("1IFXu-ybDMz5asDhDRFljB6ziE6WtPRzw9W9hodvnqLE"))
+MapTable[, id := as.numeric(id)]
+MapTable <- MapTable[!is.na(id) & !is.na(utf16string), .(id, utf16string)]
+
+NewTable <- merge(ChTable, MapTable, by = "id", all.x = TRUE)
+NewTable[!is.na(utf16string.y), utf16string.x :=utf16string.y]
+NewTable[, utf16string.y:=NULL]
+setnames(NewTable, "utf16string.x","utf16string")
+setorder(NewTable, id)
+
+pb <- progress_bar$new(format = "  replacing [:bar] :percent eta: :eta", total = nrow(NewTable))
+
+for(curId in NewTable$id){
+  pb$tick()
+  # curId <- '3364070197' # example
+  location <- stri_locate_all_fixed(pattern=paste0('\"',curId,'\":\"'), rawString)[[1]]
+  startPos <- unname(location[1,2]+1)
+  endLocations <- stri_locate_all_fixed(pattern='\",\"', rawString)[[1]][,1]
+  endPos <- unlist(endLocations[which(endLocations>startPos)[1]]-1)
+  rawString <- `stri_sub<-`(rawString, startPos, endPos, value=unlist(NewTable[id==curId,utf16string]))
+}
+rawString <- stri_replace_all_fixed(rawString, '\n','\\n')
+# save(rawString, file='modifiedString.RData')
+
+# write(rawString, "ModifiedEN")
+
+# rawString <- stri_replace_all_fixed(rawString, '\",\"', '\",\n\"')
+write(rawString, "ModifiedEN.txt")
+
+git2r::commit(message='0', all = TRUE)
+# git2r::config(repo, user.name = "Alice", user.email = "Alice@example.com")
+# git2r::push()
+endTime <- Sys.time()
+print(endTime-startTime)
