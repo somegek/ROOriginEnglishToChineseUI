16546f105cdd84adc47402cfcf06be25821e659d
somegek <somegek@gmail.com>
2020-12-02 12:38:56 +0100

    0

--- download.R
+++ download.R
@@ -5,9 +5,9 @@ configJson <- rjson::fromJSON(file = "http://roo.golitsyn.com/api/?method=config
 configLang <- unlist(lapply(configJson$translations, function(x){x$lang}))
 configData <- unlist(lapply(configJson$translations, function(x){sub('\\?.*$','',x$data)}))
 
-# dataPath <- configData[configLang=='English']
+dataPath <- configData[configLang=='English']
 # dataPath <- 'http://roo.golitsyn.com/translations/json/EN'
-dataPath <- 'https://raw.githubusercontent.com/somegek/ROOriginEnglishToChineseUI/master/OriginalEN'
+# dataPath <- 'https://raw.githubusercontent.com/somegek/ROOriginEnglishToChineseUI/master/OriginalEN'
 
 rawString <- readr::read_file(dataPath)
 
